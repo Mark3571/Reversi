b7f1b57121bd9ac4abb3243f7fef38949bcf9b66
Mark3571 <markcrooijmans14@gmail.com>
2025-10-14 09:53:05 +0200

    Class created

--- Reversi.py
+++ Reversi.py
@@ -1,1 +1,14 @@
-Reversi
+#Reversi Imperatief Programmeren
+#Gemaakt door Mark Crooijmans en Bruno Schuttelaars
+
+import tkinter as tk
+from PIL import Image, ImageDraw, ImageTk
+
+
+#Defineer de klasse voor het spel
+class ReversiGame:
+    #Constanten voor status vakje
+    EMPTY = 0 #Geen steen op veld
+    BLUE = 1 #Blauwe steen
+    RED = 2 #Rode steen
+     
